4351625ded41326c32f7b799fff3698912adb8d1
BartoszAntoniak <47859217+BartoszAntoniak@users.noreply.github.com>
2019-04-02 21:54:50 +0200

    Update Main.py
    Added special characters

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import random, re, pyperclip
 
-Characters = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','0','1','2','3','4','5','6','7','8','9']
-StrongPassword1 = re.compile(r'[QWERTYUIOPASDFGHJKLZXCVBNM]')
-StrongPassword2 = re.compile(r'[qwertyuiopasdfghjklzxcvbnm]')
-StrongPassword3 = re.compile(r'\d')    
-    
+Characters = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','(',')','-','_','+','=']
+StrongPassword1 = re.compile("[QWERTYUIOPASDFGHJKLZXCVBNM]")
+StrongPassword2 = re.compile("[qwertyuiopasdfghjklzxcvbnm]")
+StrongPassword3 = re.compile("\d")
+StrongPassword4 = re.compile("[$&+,:;=?@#|'<>.-^*()%!]")
+
 def ListToString(list):
     PasswordString = ''
     for character in list:
@@ -13,8 +14,11 @@ def ListToString(list):
     if StrongPassword1.findall(PasswordString)!= []:
         if StrongPassword2.findall(PasswordString) != []:
             if StrongPassword3.findall(PasswordString) != []:
-                pyperclip.copy(PasswordString) 
-                print(PasswordString)
+                if StrongPassword4.findall(PasswordString) != []:
+                    pyperclip.copy(PasswordString) 
+                    print(PasswordString)
+                else:
+                    PasswordGenerator()
             else:
                 PasswordGenerator()
         else:
@@ -28,7 +32,7 @@ def PasswordGenerator():
     i=1
 
     while i <= 10:
-        PasswordList.append(Characters[random.randrange(60)])
+        PasswordList.append(Characters[random.randrange(74)])
         i=i+1
     ListToString(PasswordList)
     return
